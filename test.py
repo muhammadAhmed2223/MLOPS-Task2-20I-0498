e7995bfe68f36c7bbe13d6279fc332d5a1db04e7
muhammadAhmed2223 <145197626+muhammadAhmed2223@users.noreply.github.com>
2024-02-19 09:39:36 +0500

    Create test.py

---
+++ test.py
@@ -0,0 +1,16 @@
+from main import StudentsInMLOps
+
+def test_enroll_students():
+    obj = StudentsInMLOps()
+    obj.enrollStudents(5)
+    assert obj.getTotalStrength() == 5
+
+def test_drop_students():
+    obj = StudentsInMLOps()
+    obj.enrollStudents(10)
+    obj.dropStudents(3)
+    assert obj.getTotalStrength() == 7
+
+def test_get_class_name():
+    obj = StudentsInMLOps()
+    assert obj.getClassName() == "StudentsInMLOps"
